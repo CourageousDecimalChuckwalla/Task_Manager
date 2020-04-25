606956bfce35de7487e09686cfbfe4355950212b
CourageousDecimalChuckwalla <CourageousDecimalChuckwalla@aaathats3as.com>
2020-04-25 15:40:55 -0700

    Initial testing

--- pyTaskManager.py
+++ pyTaskManager.py
@@ -1,15 +1,5 @@
 #Initial Commit
 #test
 
-import os
 import sys
-print("test")
-
-#check that module(s) were imported
-#check for multiple modules using a list
-# modulenames = ["os"]
-# for x in modulenames:
-#     if modulenames[x] not in sys.modules:
-#         print(modulenames[x], " is not imported.")
-
-#check for one module at a time
+    #contains os module
