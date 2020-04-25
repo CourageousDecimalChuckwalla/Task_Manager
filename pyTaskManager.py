4c5960ae945a681cbed48f50d1debe3c3fa0c063
CourageousDecimalChuckwalla <CourageousDecimalChuckwalla@aaathats3as.com>
2020-04-25 16:32:02 -0700

    Basic printing of processes with name and PID

--- pyTaskManager.py
+++ pyTaskManager.py
@@ -1,5 +1,34 @@
-#Initial Commit
-#test
+# Initial Commit
+# test
 
 import sys
-    #contains os module
+# sys module contains os module
+import pandas as pd
+import psutil
+import datetime as dt
+import time
+
+
+def get_processes():
+    processes = []
+
+    for process in psutil.process_iter():
+        with process.oneshot():
+            pid = process.pid
+            name = process.name()
+
+            if pid == 0:  # Filter out System Idle Process
+                continue
+
+        processes.append({
+            'pid': pid,
+            'name': name
+        })
+
+    return processes
+
+
+if __name__ == "__main__":
+    for x in get_processes():
+        print(x)
+
