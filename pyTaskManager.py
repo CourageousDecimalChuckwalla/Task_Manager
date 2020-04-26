4903414f452e65f7054cbef5cfbd0989ac21199d
CourageousDecimalChuckwalla <CourageousDecimalChuckwalla@aaathats3as.com>
2020-04-25 18:08:35 -0700

    Prints PID, name, creation time

--- pyTaskManager.py
+++ pyTaskManager.py
@@ -1,13 +1,13 @@
-# Initial Commit
-# test
-
 import sys
 # sys module contains os module
 import pandas as pd
 import psutil
-import datetime as dt
+from datetime import datetime
 import time
 
+
+# Outputs name, pid, cpu_usage, memory_usage, read/write bytes, status, create_time, thread, cores
+# Implemented: name, pid, creation time
 
 def get_processes():
     processes = []
@@ -20,9 +20,11 @@ def get_processes():
             if pid == 0:  # Filter out System Idle Process
                 continue
 
+            create_time = datetime.fromtimestamp(process.create_time())
         processes.append({
             'pid': pid,
-            'name': name
+            'name': name,
+            'time': create_time.strftime("%c")
         })
 
     return processes
@@ -32,3 +34,8 @@ if __name__ == "__main__":
     for x in get_processes():
         print(list(x.values()))
 
+
+# References
+# https://www.thepythoncode.com/article/make-process-monitor-python
+#   Raw Code: https://www.thepythoncode.com/code/make-process-monitor-python
+#   Github: https://github.com/x4nth055/pythoncode-tutorials/tree/master/general/process-monitor
